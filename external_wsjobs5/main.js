780196de663ed7bcf61ebcf984c5940f77b06da7
alessandrooCosta <alessandrocosta103@gmail.com>
2025-10-10 18:35:20 -0300

    cria nova customização de tela

--- external_wsjobs5/main.js
+++ external_wsjobs5/main.js
@@ -1,17 +1,34 @@
-// USAR PARA TESTES E DESENVOLVIMENTO
-
-Ext.define("EAM.custom.external_wsjobs", {
-    extend: "EAM.custom.AbstractExtensibleFramework",
-    getSelectors: function () {
+Ext.define('EAM.custom.external_wsjobs', {
+    extend: 'EAM.custom.AbstractExtensibleFramework',
+    getSelectors: function() {
+        console.log('WSJOBS5 carregado');
         return {
-            '[extensibleFramework] [tabName=HDR] uxcombobox[name=workorderstatus]': {
-                customonblur: function (field) {
-                    var vFormPanel = EAM.Utils.getCurrentTab().getFormPanel();
-                    var vStatus = vFormPanel.getFld('workorderstatus').rawValue;
-                    vFormPanel.setFldValue('udfchar01', vStatus, true);
-                }
+            
+            '[extensibleFramework] [tabName=HDR] [name=organization]': {
+    customonblur: function(field, lastValues) {
+        var vFormPanel = EAM.Utils.getCurrentTab().getFormPanel();
+        var orgValue = vFormPanel.getFldValue('organization');
+        
+        if (orgValue && orgValue.trim() !== '') {
+            console.log('Organização selecionada, aplicando regras...'); // log para debug
+            var vFieldsObj = {};
+            vFieldsObj["description"] = "protected";
+            vFieldsObj["servicecategory"] = "required";
+            vFieldsObj["serviceproblemcode"] = "required";
+            vFieldsObj["udfnote01"] = "protected";
+            try {
+                var fieldsAndButtons = vFormPanel.getForm().getFieldsAndButtons();
+                EAM.Builder.setFieldState(vFieldsObj, fieldsAndButtons);
+                console.log('Regras aplicadas');
+            } catch (error) {
+                console.error('Erro:', error);
             }
+        }
+ 
+    }
+},
+
+
         };
     }
-  });
-  
+});
